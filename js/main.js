fcb49251c572a80f02d37ea178967efadbd25d13
Antoine Robert <antoine.al.robert@gmail.com>
2022-01-19 16:12:16 +0100

    3 - The main character sprite

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function Hero (game, x, y) {
+  // call Phaser.Sprite constructor
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  this.anchor.set(0.5, 0.5);
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
+
 const PlayState = {};
 
 // Loading game assets
@@ -12,6 +23,8 @@ PlayState.preload = function () {
   this.game.load.image('grass:4x1', 'images/grass_4x1.png');
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+
+  this.game.load.image('hero', 'images/hero_stopped.png');
 }
 
 // create game entities and set up world here
@@ -24,10 +37,17 @@ PlayState._loadLevel = function (data) {
   // console.log(data);
   // spawn all platforms
   data.platforms.forEach(this._spawnPlatform, this);
+  this._spawnCharacters({hero: data.hero});
 }
 
 PlayState._spawnPlatform = function (platform) {
   this.game.add.sprite(platform.x, platform.y, platform.image);
+}
+
+PlayState._spawnCharacters = function (data) {
+  // spawn hero and enemies
+  this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+  this.game.add.existing(this.hero); 
 }
 
 window.onload = function () {
