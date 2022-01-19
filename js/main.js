1100668c25f6d2d5e8e343edacbf2c5386ef30b0
Antoine Robert <antoine.al.robert@gmail.com>
2022-01-19 16:23:57 +0100

    5 - Moving sprites with physics

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function Hero (game, x, y) {
   // call Phaser.Sprite constructor
   Phaser.Sprite.call(this, game, x, y, 'hero');
   this.anchor.set(0.5, 0.5);
+  this.game.physics.enable(this);
+  this.body.collideWorldBounds = true;
 }
 
 // inherit from Phaser.Sprite
@@ -9,7 +11,8 @@ Hero.prototype = Object.create(Phaser.Sprite.prototype);
 Hero.prototype.constructor = Hero;
 
 Hero.prototype.move = function (direction) {
-  this.x += direction * 2.5; // 2.5 pixels each frame
+  const SPEED = 200;
+  this.body.velocity.x = direction * SPEED;
 };
 
 
@@ -73,6 +76,9 @@ PlayState._handleInput = function () {
   }
   else if (this.keys.right.isDown) { // move hero right
       this.hero.move(1);
+  }
+  else {
+    this.hero.move(0);
   }
 }
 
